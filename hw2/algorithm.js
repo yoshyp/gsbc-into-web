90a133238099d46a6ef44178458eb78f50d714e5
Josh Parayno <joshua.parayno@students.gsbc.edu>
2022-03-17 23:33:03 -0700

    hw2:trying to fix js

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -1,10 +1,1 @@
-// randomly generated N = 100 length array 1 <= A[N] <= 1000
-Array.from({length: 1000}, () => Math.floor(Math.random() * 1000));
-
-// Print the results by appending paragraph nodes to the document body.
-for (var result of results)
-{
-    let textNode = document.createElement("p")
-    textNode.textContent = result
-    document.body.appendChild(textNode)
-}
+[...Array(100)].map(e=>~~(Math.random()*1000))
