91487a15ddea4e0aa7b3bfd43fa7895534450d53
Josh Parayno <joshua.parayno@students.gsbc.edu>
2022-03-25 08:27:45 -0700

    HW2: working on JS

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -3,10 +3,10 @@ let total = 0
 let p1 = document.createElement("p")
 p1.textContent = ".";
 document.body.appendChild(p1)
-for (let x=1; x <= 100; x++) {
+for (let x = 1; x <= 100; x++) {
     let rndNum = parseInt(Math.random() * (1000 -1) +1)
     let p = documet.createElement("p")
     p.textContent = rndNum
     //document.getElementbyId("numbers").appendChild(p)
-    document.body.appendChild(p1)
+    document.body.appendChild(p)
 }
