ac351c321ea346a3e9c0e485d8e446c25f3c505d
Josh Parayno <joshua.parayno@students.gsbc.edu>
2022-03-25 08:50:57 -0700

    Hw2:fixing js

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -5,7 +5,7 @@ p1.textContent = ".";
 document.body.appendChild(p1)
 for (let x = 1; x <= 100; x++) {
     let rndNum = parseInt(Math.random() * (1000 -1) + 1)
-    let p = documet.createElement("p")
+    let p = document.createElement("p")
     p.textContent = rndNum
     raNums.push(rndNum)
     //document.getElementbyId("numbers").appendChild(p)
